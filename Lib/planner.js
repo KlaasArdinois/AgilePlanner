ad6c3ef5a5cbb3ef3a7653d38d43c3113b46e0c4
Klaas Ardinois <klaas.ardinois@gmail.com>
2013-10-19 13:13:11 +0100

    added mean and standard dev

--- Lib/planner.js
+++ Lib/planner.js
@@ -7,19 +7,27 @@ function plan(velocities, sprints, cycles) {
              var index = math.randomInt(0,velocities.length-1);
              totalPoints += velocities[index];         
         }
-   
-        if(typeof resultSet[totalPoints] === 'undefined') {
-            resultSet[totalPoints] = 0;
-        }
-        resultSet[totalPoints]++;
-	}
 
-    for(i = 0; i < resultSet.length; i++) {
-        if(typeof resultSet[i] === 'undefined') {
-            resultSet[i] = 0;
-        }
+        resultSet[i] = totalPoints;
+   
     }
     return resultSet;
 }
 
+function mean(input) {
+    var sum = 0;
+    for(i = 0; i < input.length; i++) {
+        sum += input[i];
+    }
+    return sum/input.length;
+}
 
+function standardDev(input) {
+    var m = mean(input);
+    var squaredDifSum = 0;
+    for(i = 0; i < input.length; i++) {
+        squaredDifSum += Math.pow((input[i] - m), 2);
+    }
+
+    return Math.sqrt(squaredDifSum/input.length);
+}
